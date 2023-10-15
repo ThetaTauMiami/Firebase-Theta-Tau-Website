0c6d0cb0a170d2085a55b94054b1757b13330c2d
Nicholas Wheeler <wheelenj@miamioh.edu>
2023-10-15 14:57:06 -0400

    Added initial points functionlity

---
+++ public/points.js
@@ -0,0 +1,226 @@
+// This JavaScript document is for the account page only, as other JavaScript files will be used for login.html and the static pages
+// This JavaScript file handles logged in users, admin users, and provides a way for logged out users to exit the page
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
+import { AuthErrorCodes } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
+import { getAuth, onAuthStateChanged, signOut, signInWithEmailAndPassword, connectAuthEmulator } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
+
+// Initalize constants that the JS will use to interact with HTML elements based on user authentication state
+
+
+//Logged-In User Sections
+const divFullUser = document.querySelector('#fullUser')
+const loggedInNavbar = document.querySelector('#loggedInNavbar')
+const logoutHeader = document.querySelector('#logoutHeader')
+
+//Logged-Out User Sections
+const divSignedOutUser = document.querySelector('#signedOutUser')
+
+// Return to Login Button
+const btnLoginReturn = document.querySelector('#btnLoginReturn')
+
+// Points Update Button (ADMINS ONLY)
+const updatePointsSection = document.querySelector('#updatePointsSection')
+
+///////////////// Firebase Initialization ////////////////
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyCuS3TWRtitOxxjJ3gyb-lxH2kmu2N0Ij8",
+    authDomain: "thetataumiamiuniversity.firebaseapp.com",
+    projectId: "thetataumiamiuniversity",
+    storageBucket: "thetataumiamiuniversity.appspot.com",
+    messagingSenderId: "752928414181",
+    appId: "1:752928414181:web:d70dbd3f4ed11077e7b70c",
+    measurementId: "G-BTNR03FCB4"
+});
+
+//////// Firestore Variables (Database) //////////
+const db = firebase.firestore();
+const configRef = db.collection('config').doc('roles');
+let usersRef; // Reference to the document or collection we want to access
+let unsubscribe; // Query handler for user information check
+let adminCheck; // Query handler for admin uid check
+
+// Core constant variables used to regulate a user's authenticated 'state'
+const auth = firebase.auth();
+const authentication = getAuth(firebaseApp);
+
+///////////////////// User Authentication ////////////////////////
+// Monitor user Authentication state, this will change website contents using javascript functions
+const monitorAuthState = async () => {
+    onAuthStateChanged(authentication, user => {
+      if (user) {
+        console.log("Logged In")
+      }
+      else {
+        console.log("Not Logged In")
+      }
+    })
+}
+
+monitorAuthState();
+
+// Log out
+const logout = async () => {
+  await signOut(auth);
+}
+
+const logoutExit = () => {
+  logout(); // Ensure the user is signed out
+  window.location.replace('login.html'); // Send the user back to the Login page
+}
+
+
+////////////////////// FUNCTIONS ///////////////////////////////////
+// Function to check if a user owns any items in the Cloud Firestore database
+function userOwnsSomething(userId) {
+  return db.collection("userData")
+    .where("uid", "==", userId)
+    .get()
+    .then((querySnapshot) => {
+      return !querySnapshot.empty; // returns true if user owns things, false otherwise
+    })
+    .catch((error) => {
+      return false; // return false if there was an error [nothing owned]
+    });
+}
+
+// This button will return a user back to the "Login" page and ensure that they are logged out as well
+btnLoginReturn.addEventListener("click", logoutExit);
+// Normal Logout Button
+// btnLogout.addEventListener("click", logoutExit);
+
+// Logout header clicked
+logoutHeader.addEventListener("click", logoutExit);
+
+// This button will take the user to the photo upload page
+// btnUploadPhoto.addEventListener("click", goToPhotoUpload);
+
+// This button will take the user to the profile update page
+// btnUpdateProfile.addEventListener("click", goToProfileUpdate);
+
+// This authentication listener regulates what the user sees
+// on the page depending on the authentication state.
+auth.onAuthStateChanged(user => {
+  if (user) {
+      // SIGNED IN
+      // Create a reference to the database user collection
+      usersRef = db.collection('userData');
+      // Hide Signed-Out Content
+      divSignedOutUser.hidden = true;
+      // Checks to see if the current user has setup their account (Not first sign-in)
+      userOwnsSomething(user.uid)
+          .then((userOwnsThings) => {
+            console.log("User owns things? ", userOwnsThings);
+            if (!userOwnsThings) { // If the user hasn't setup their account
+              // Send them back to the account setup page first
+              window.location.replace('account.html');
+            } else {
+              // Query the admin information
+              adminCheck = configRef.get().then(doc => {
+                  if (doc.exists) {
+                    const admins = doc.data().admins;
+                    if (admins && admins.includes(user.uid)) {
+                        // User is an admin
+                        usersRef.get().then(querySnapshot => {
+                            querySnapshot.forEach(doc => {
+                                const userData = doc.data();
+                                const userRow = takesUserCreatesHTML(userData);
+                                document.getElementById('memberList').appendChild(userRow);
+                            });
+                        }).catch(error => {
+                            console.error('Error getting users:', error);
+                        });
+                    } else {
+                        // User is not an admin, go back to account page
+                        window.location.replace('account.html');
+                    }
+                  }
+                })
+                .catch(error => {
+                  console.error('Error getting document:', error);
+                });
+            }
+      });
+
+  } else {
+      // NOT SIGNED IN
+      // Send user back to sign in
+      logoutExit();
+      //Hide Signed-In Sections
+
+      divFirstLoginPrompt.hidden = true;
+      divFirstLoginForm.hidden = true;
+      divFullUser.hidden = true;
+      loggedInNavbar.hidden = true;
+      // Show Logged-Out Section
+      divSignedOutUser.hidden = false;
+      // Unsubscribe listening streams for no memory leak when user signs out
+      unsubscribe && unsubscribe();
+  }
+});
+
+
+function takesUserCreatesHTML(userDocument) {
+  let element = document.createElement("div")
+  element.setAttribute("class", "row")
+  element.setAttribute("style", "padding:10px")
+  element.innerHTML =
+      `<div class="col-lg-2">
+        <img src="${userDocument.pictureLink}" style="width:100%" />
+      </div>
+      <div class="col-lg-2">
+        <h5>${userDocument.firstname} ${userDocument.lastname}</h5>
+      </div>
+      <div class="col-lg-1">
+          <input type="number" id="brotherhoodPoints-${userDocument.uid}" name="brotherhoodPoints" step="1" min="0" value="${userDocument.brotherhoodPoints}">
+      </div>
+      <div class="col-lg-1">
+          <input type="number" id="servicePoints-${userDocument.uid}" name="servicePoints" step="1" min="0" value="${userDocument.servicePoints}">
+      </div>
+      <div class="col-lg-1">
+        <input type="number" id="pdPoints-${userDocument.uid}" name="pdPoints" step="1" min="0" value="${userDocument.pdPoints}">
+      </div>
+      <div class="col-lg-1">
+        <input type="number" id="generalPoints-${userDocument.uid}" name="generalPoints" step="1" min="0" value="${userDocument.generalPoints}">
+      </div>
+      <div class="col-lg-2">
+        <input type="checkbox" id="deiPoint-${userDocument.uid}" name="deiPoint" checked="${userDocument.deiFulfilled}" value="true">
+      </div>
+      <div class="col-2">
+        <button class="btn btn-warning updatePoints-btn">Update</button> 
+      </div>
+      `;
+    
+      const updatePointsBtn = element.querySelector('.updatePoints-btn');
+      updatePointsBtn.addEventListener('click', () => {
+        updateUserPoints(userDocument);
+      });
+
+      // Send the formatted element back to be appended
+      return element;
+}
+
+function updateUserPoints(userDocument) {
+  const usersRef = db.collection("userData");
+  const user_brotherhoodPoints = document.querySelector(`#brotherhoodPoints-${userDocument.uid}`).value;
+  const user_servicePoints = document.querySelector(`#servicePoints-${userDocument.uid}`).value;
+  const user_pdPoints = document.querySelector(`#pdPoints-${userDocument.uid}`).value;
+  const user_generalPoints = document.querySelector(`#generalPoints-${userDocument.uid}`).value;
+  const user_deiPoint = document.querySelector(`#deiPoint-${userDocument.uid}`).value;
+
+  const query = usersRef.where('uid', '==', userDocument.uid);
+      query.get()
+        .then((snapshot) => {
+          snapshot.forEach((doc) => {
+            usersRef.doc(doc.id).update({ brotherhoodPoints: parseInt(user_brotherhoodPoints) });
+            usersRef.doc(doc.id).update({ servicePoints: parseInt(user_servicePoints) });
+            usersRef.doc(doc.id).update({ pdPoints: parseInt(user_pdPoints) });
+            usersRef.doc(doc.id).update({ generalPoints: parseInt(user_generalPoints) });
+            usersRef.doc(doc.id).update({ deiFulfilled: user_deiPoint });
+          });
+        })
+        .catch((error) => {
+          console.log("Error getting documents: ", error);
+        });
+}
+
+
